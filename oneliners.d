5d8fd6fb915818d547ae6bde970d4f06f373888c
Super-User <root@AntonTest.local>
2012-11-07 19:08:47 +0000

    Garbage Collection

--- oneliners.d
+++
@@ -1,8 +0,0 @@
-dtrace -n node*:::http-server-request{
-    printf(.%s of %s from %s\n., args[0]->method,
-        args[0]->url, args[1]->remoteAddress)}
-
- dtrace -n http-server-request.{@[args[1]->remoteAddress] = count()}.
-
- dtrace -n gc-start.{self->ts = timestamp}. \
-    -n gc-done./self->ts/{@ = quantize(timestamp - self->ts)}
