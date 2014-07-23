b9f99ad19af1a5cef7d5ea98839f8c2f2cadb1f2
No9 <mrcowsy@gmail.com>
2014-07-23 23:43:55 +0100

    Added dtrace-provider dep for samples

--- node_modules/dtrace-provider/libusdt-build.sh
+++ node_modules/dtrace-provider/libusdt-build.sh
@@ -4,29 +4,29 @@
 #
 unset MAKEFLAGS
 
-# Attempt to use the appropriate node binary - respect npm if that's
-# how we're being built, otherwise use node from PATH
-if [ -z $npm_config_prefix ]; then
-  NODE=`which node`
-else
-  NODE="$npm_config_prefix/bin/node"
-fi
-echo "Using node: $NODE"
-
 # Ask node what arch it's been built for, and build libusdt to match.
+#
+# We use node from the path; npm will have adjusted PATH for us if
+# necessary, otherwise we assume the user did so when building by
+# hand.
 #
 # (this will need to change at the point that GYP is able to build
 # node extensions universal on the Mac - for now we'll go with x86_64
 # on a 64 bit Mac, because that's the default architecture in that
 # situation).
 #
-ARCH=`$NODE libusdt-arch.js`
+ARCH=`node libusdt-arch.js`
 echo "Building libusdt for ${ARCH}"
 export ARCH
 
 # Respect a MAKE variable if set
 if [ -z $MAKE ]; then
-  MAKE=make
+  # Default to `gmake` first if available, because we require GNU make
+  # and `make` isn't GNU make on some plats.
+  MAKE=`which gmake`
+  if [ -z $MAKE ]; then
+    MAKE=make
+  fi
 fi
 
 # Build.
